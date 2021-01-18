c10d1a3dc3d24ee9747d823d4e73fc8473e83779
David <caico@fibertel.com.ar>
2021-01-18 10:57:45 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+
+- Menú prncipal  login o registro.
+- Registro , crea un nuevo usuario pala la BBDD.
+- Login se ingresa con un usuario y se muestra el menú nota, mostrar notas, borrar notas.
+
+"""
